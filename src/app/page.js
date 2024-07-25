82a031ee6950257323b76ec39c05b9c6ae5b2b63
TracerStudySmekdapapa <88016676+Thebibii@users.noreply.github.com>
2024-07-25 19:20:59 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,21 +6,21 @@ import Project from "../components/pages/Project";
 import Skill from "../components/pages/Skill";
 import Footer from "../components/pages/Footer";
 
-async function getData() {
-  const res = await fetch("https://thebibie.vercel.app/api/data", {
-    cache: "no-cache",
-  });
-  const data = await res.json();
-  return data;
-}
+// async function getData() {
+//   const res = await fetch("https://thebibie.vercel.app/api/data", {
+//     cache: "no-cache",
+//   });
+//   const data = await res.json();
+//   return data;
+// }
 
 export default async function App() {
-  const data = await getData();
+  // const data = await getData();
   return (
     <Fragment>
-      <Home data={data.home} />
+      {/* <Home data={data.home} />
       <About data={data.about} />
-      <Experience data={data.experience} />
+      <Experience data={data.experience} /> */}
       {/* <Project data={data.project} /> */}
       <div className="bg-[#233831] w-screen lg:px-28 md:px-16 p-6 xl:px-52">
         <Skill />
